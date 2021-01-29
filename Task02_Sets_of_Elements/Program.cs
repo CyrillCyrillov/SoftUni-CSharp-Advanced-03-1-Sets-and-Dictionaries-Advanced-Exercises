33400b03dcf5a5f4fdbc5536a4aff6b6ee148be7
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-30 01:27:26 +0200

    Add files via upload

---
+++ Task02_Sets_of_Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Task02_Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> firstSet = new HashSet<string>();
+            HashSet<string> secondtSet = new HashSet<string>();
+
+            int[] setsVolume = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            int nFirstSet = setsVolume[0];
+            int mSecondSet = setsVolume[1];
+
+            for (int i = 1; i <= nFirstSet; i++)
+            {
+                firstSet.Add(Console.ReadLine());
+            }
+
+            for (int i = 1; i <= mSecondSet; i++)
+            {
+                secondtSet.Add(Console.ReadLine());
+            }
+
+            foreach (string element in firstSet)
+            {
+                if(secondtSet.Contains(element))
+                {
+                    Console.Write($"{element} ");
+                }
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
