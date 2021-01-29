e414ba1db26fef9ce26faa47e2f5932ef3ca8865
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-30 01:29:06 +0200

    Add files via upload

---
+++ Task04_Even Times/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task04_Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> countNumbers = new Dictionary<string, int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                string nextNumber = Console.ReadLine();
+                if (!countNumbers.ContainsKey(nextNumber))
+                {
+                    countNumbers.Add(nextNumber, 1);
+                }
+                else
+                {
+                    countNumbers[nextNumber]++;
+                }
+            }
+
+            foreach (var element in countNumbers)
+            {
+                if((element.Value) % 2 == 0)
+                {
+                    Console.WriteLine(element.Key);
+                    break;
+                }
+            }
+            
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
