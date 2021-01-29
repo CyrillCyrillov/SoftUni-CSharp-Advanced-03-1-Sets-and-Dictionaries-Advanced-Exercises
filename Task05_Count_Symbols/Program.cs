1830392dc1917ce55043c9f3f5265b938fc0eb3e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-30 01:29:58 +0200

    Add files via upload

---
+++ Task05_Count_Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task05_Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> symbolsCount = new SortedDictionary<char, int>();
+
+            string inputString = Console.ReadLine();
+
+            foreach (char element in inputString)
+            {
+                if(!symbolsCount.ContainsKey(element))
+                {
+                    symbolsCount.Add(element, 1);
+                }
+                else
+                {
+                    symbolsCount[element]++;
+                }
+                
+            }
+
+            foreach (var element in symbolsCount)
+            {
+                Console.WriteLine($"{element.Key}: {element.Value} time/s");
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
